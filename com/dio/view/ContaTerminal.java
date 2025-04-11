0f34bce358b9c44e5b152bf69eace7a980283c00
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-04-11 10:28:26 -0300

    Realizando a atividade

---
+++ com/dio/view/ContaTerminal.java
@@ -0,0 +1,36 @@
+package com.dio.view;
+
+import com.dio.model.ContaCorrente;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Olá, bem vindo ao sistema do Banco");
+
+        System.out.println("Por favor, digite o número da conta:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Agora digite a agência");
+        String agencia = scanner.next();
+
+        System.out.println("Informe o seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("Agora informe o saldo inicial da conta:");
+        float saldo = scanner.nextFloat();
+
+        ContaCorrente contaCorrente = new ContaCorrente(numero, agencia, nome, saldo);
+
+        System.out.println("Olá "+contaCorrente.getNomeCliente()+", obrigado por criar uma conta em nosso banco, sua agência é "+contaCorrente.getAgencia()+", conta "+contaCorrente.getNumero()+" e seu saldo "+contaCorrente.getSaldo()+" já está disponível para saque");
+
+
+
+
+    }
+
+}
